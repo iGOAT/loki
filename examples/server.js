df9befbb1e9f8dc360a2d7a89fa1e1b7581cd457
Joey Nenni <joey.nenni@gmail.com>
2011-08-12 13:01:41 -0700

    fixed example

--- examples/server.js
+++ examples/server.js
@@ -1,1 +1,5 @@
-require('loki').start();
+// this line should be used if the example was in the same directory
+// as the 'node_modules' directory containing the 'loki' directory
+// require('loki').start();
+
+require('../lib/loki').start();
